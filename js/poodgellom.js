c93b8ce6d375e41cc1f6579cdac0fa9ad2604b01
Luke Garwood <luke_gar@hotmail.com>
2016-04-24 09:23:04 -0400

    added nav show/hide on scroll

--- js/poodgellom.js
+++ js/poodgellom.js
@@ -4,13 +4,24 @@
 
 function worm(){
     screenWidth = (window.innerWidth + 300) + "px";
-    
-  $( "#worm" ).animate({ "left": '-=' + screenWidth + ''}, 100000 );
-     
-    
-   
- }
+    $( "#worm" ).animate({ "left": '-=' + screenWidth + ''}, 100000 );
+}
 
 worm();
+
+
+$(window).scroll(
+    {
+        previousTop: 0
+    }, 
+    function () {
+    var currentTop = $(window).scrollTop();
+    if (currentTop < this.previousTop) {
+        $(".custom-main-menu").show();
+    } else {
+        $(".custom-main-menu").hide();
+    }
+    this.previousTop = currentTop;
+})
 
 
