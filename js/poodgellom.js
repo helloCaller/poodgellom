8612b322711ec139462dcca13d43ca79eac68969
Luke Garwood <luke_gar@hotmail.com>
2016-04-25 00:14:25 -0400

    mobile revisions of logo and navbar

--- js/poodgellom.js
+++ js/poodgellom.js
@@ -6,14 +6,13 @@ function worm(){
     screenWidth = (window.innerWidth + 300) + "px";
     $( "#worm" ).animate({ "left": '-=' + screenWidth + ''}, 100000 );
 }
-
 worm();
 
-var navbar = {
-    menuOut: "false"
-}
 
-$(".custom-main-menu").click(function(){
+var navbar = {
+    menuOut: "false",
+}
+$(".navbar-toggle").click(function(){
     if(navbar.menuOut == "false"){
         navbar.menuOut = "true";
     } else {
@@ -22,8 +21,7 @@ $(".custom-main-menu").click(function(){
     console.log(navbar.menuOut);
 });
 
-
-    $(window).scroll(
+$(window).scroll(
         {
             previousTop: 0
         }, 
@@ -31,12 +29,16 @@ $(".custom-main-menu").click(function(){
         var currentTop = $(window).scrollTop();
         if (currentTop < this.previousTop) {
             $(".custom-main-menu").show();
-        } else {
-            if(navbar.menuOut == "false"){
+        } else if(navbar.menuOut == "false" && currentTop >=20){
             $(".custom-main-menu").hide();
             }
-        }
+        
             this.previousTop = currentTop;
+
         })
+    
+    
+
+
 
 
